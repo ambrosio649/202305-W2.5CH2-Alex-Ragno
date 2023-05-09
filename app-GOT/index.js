0ea66f30c5079a6b8c4f769136f3062f307bc252
AlexRagno <alexragno4@gmail.com>
2023-05-09 20:20:46 +0200

    Add index.js

---
+++ app-GOT/index.js
@@ -0,0 +1,5 @@
+import { Character } from "./character.js";
+import { Warrior } from "./warrior.js";
+import { Adviser } from "./adviser.js";
+import { King } from "./king.js";
+import { Squire } from "./squire.js";
